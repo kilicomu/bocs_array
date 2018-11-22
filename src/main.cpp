605144b07fe40711bfe764663a25d8d85269b374
Killian Murphy <killian.murphy@york.ac.uk>
2018-11-22 11:41:13 +0000

    Update some names

--- src/main.cpp
+++ src/main.cpp
@@ -10,19 +10,11 @@
  * @brief  Code that will run before the main execution loop.
  ******************************************************************************/
 void setup() {
-  Serial.begin(9600);
-  while (!Serial) {}
-  Serial.println("INFO: INITIALISED SERIAL");
 
-  i2c_init();
-  Serial.println("INFO: INITIALISED I2C");
-
-  uint8_t i2c_status = i2c_select_channel(NO_SENSORS);
-  Serial.print("INFO: I2C STATUS IS ");
-  Serial.println(i2c_status);
 }
 /***************************************************************************//**
  * @brief  The main execution loop.
  ******************************************************************************/
 void loop() {
+
 }
