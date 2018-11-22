6650f6972be123181816b1be5ca63af26e8725f9
Killian Murphy <killian.murphy@york.ac.uk>
2018-11-22 10:05:27 +0000

    Add unfinished skeleton of I2C library

---
+++ lib/BOCS_I2C/BOCS_I2C.h
@@ -0,0 +1,44 @@
+/****************************************************************************//*
+ * @file    BOCS_I2C.h
+ * @author  Killian Murphy <killian.murphy@york.ac.uk>
+ * @date    22 November 2018
+ * @brief   Usefully-named wrapper functions around Arduino Wire functions.
+ ******************************************************************************/
+#ifndef __BOCS_I2C_H__
+#define __BOCS_I2C_H__
+/****************************************************************************//*
+ * @brief  Configuration macros specifying I2C addresses of slave devices.
+ *
+ * I2C_MUX_ADDR is the I2C address of the TCA9548A.
+ *
+ * SENSOR_N_ADDR is the I2C address of sensors 1 through three on each of the
+ * TCA9548A channels.
+ ******************************************************************************/
+#define I2C_MUX_ADDR 0x70
+#define SENSOR_1_ADDR 0x4A
+#define SENSOR_2_ADDR 0x4B
+#define SENSOR_3_ADDR 0x48
+/****************************************************************************//*
+ * @brief  Configuration macros associating TCA9548A channel numbers with types
+ *         of sensors attached.
+ ******************************************************************************/
+#define NO_SENSORS 0x01
+#define NO2_SENSORS 0x02
+#define OX_SENSORS 0x03
+#define CO_SENSORS 0x04
+#define CO2_SENSORS 0x05
+/******************************************************************************/
+#include <Arduino.h>
+#include <Wire.h>
+/****************************************************************************//*
+ * @brief  Initialise I2C bus, joining as master.
+ ******************************************************************************/
+void i2c_init(void);
+/****************************************************************************//*
+ * @brief  Select a TCA9548A channel.
+ *
+ * @param[in]  channel  The channel to select, in range 0-8.
+ ******************************************************************************/
+uint8_t i2c_select_channel(uint8_t channel);
+/******************************************************************************/
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,14 @@
  * @brief   Read data from attached sensors and send over USB serial.
  ******************************************************************************/
 #include <Arduino.h>
+#include <BOCS_I2C.h>
 /***************************************************************************//**
  * @brief  Code that will run before the main execution loop.
  ******************************************************************************/
 void setup() {
-  Serial.begin(9600);
-  while (!Serial) {}
-
-  Serial.println("INFO: SERIAL INITIALISED");
 }
 /***************************************************************************//**
  * @brief  The main execution loop.
  ******************************************************************************/
 void loop() {
-  Serial.println("INFO: SERIAL IS WORKING");
-  delay(1000);
 }
