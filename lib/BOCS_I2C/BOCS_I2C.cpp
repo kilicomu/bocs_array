30cad5b6fbb8926f65c46c74e98e2ba1302d6f35
Killian Murphy <killian.murphy@york.ac.uk>
2018-11-22 10:31:01 +0000

    Add some I2C implementations; add Adafruit ADS1X15 library

---
+++ lib/BOCS_I2C/BOCS_I2C.cpp
@@ -0,0 +1,18 @@
+/****************************************************************************//*
+ * @file    BOCS_I2C.cpp
+ * @author  Killian Murphy <killian.murphy@york.ac.uk>
+ * @date    22 November 2018
+ * @brief   Usefully-named wrapper functions around Arduino Wire functions.
+ ******************************************************************************/
+#include "BOCS_I2C.h"
+/******************************************************************************/
+void i2c_init(void) {
+  Wire.begin();
+}
+/******************************************************************************/
+uint8_t i2c_select_channel(uint8_t channel) {
+  Wire.beginTransmission(I2C_MUX_ADDR);
+  Wire.write(1 << channel);
+  return Wire.endTransmission();
+}
+/******************************************************************************/
--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,16 @@
  * @brief  Code that will run before the main execution loop.
  ******************************************************************************/
 void setup() {
+  Serial.begin(9600);
+  while (!Serial) {}
+  Serial.println("INFO: INITIALISED SERIAL");
+
+  i2c_init();
+  Serial.println("INFO: INITIALISED I2C");
+
+  uint8_t i2c_status = i2c_select_channel(NO_SENSORS);
+  Serial.print("INFO: I2C STATUS IS ");
+  Serial.println(i2c_status);
 }
 /***************************************************************************//**
  * @brief  The main execution loop.
