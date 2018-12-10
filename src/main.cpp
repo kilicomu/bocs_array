0b094827d68c61f4678626a830ba9f87a21ea67a
Killian Murphy <killian.murphy@york.ac.uk>
2018-12-10 13:41:22 +0000

    Demonstration version of instrument code
    I believe that channel 4 of the I2C multiplexer board is not functioning
    correctly. This should be replaced for testing after this demonstration.
    For now, we can simply ignore NO2 and plot up the other values.

--- src/main.cpp
+++ src/main.cpp
@@ -29,70 +29,50 @@ int16_t MOS_BUFFER[MOS_BUFFER_SIZE];
  ******************************************************************************/
 void setup() {
   serial_init(9600);
-  Serial.println("INFO: INITIALISED USB SERIAL");
-  
   i2c_init_bus();
-  Serial.println("INFO: INITIALISED I2C BUS AND JOINED AS MASTER");
-
   rtc_init(rtc);
-  DateTime current_time = rtc.now();
-  Serial.print("INFO: INITIALISED RTC - THE TIME IS NOW ");
-  Serial.print(current_time.hour());
-  Serial.print(':');
-  Serial.print(current_time.minute());
-  Serial.print(':');
-  Serial.println(current_time.second());
-
   i2c_select_channel(MOS_SENSORS);
   MOS_ADCS.init_all();
-  Serial.println("INFO: INITIALISED MOS SENSOR ADCS");
-
-  for (uint8_t i = NO_SENSORS; i < CO2_SENSORS; ++i) {
+  for (uint8_t i = NO_SENSORS; i < NO2_SENSORS; ++i) {
     i2c_select_channel(i);
     ELECTROCHEM_ADCS.init_all();
   }
-  Serial.println("INFO: INITIALISED ELECTROCHEM ADCS");
-
   i2c_select_channel(CO2_SENSORS);
   CO2_ADCS.init_all();
-  Serial.println("INFO: INITIALISED CO2 ADCS");
-
   i2c_select_channel(MOS_SENSORS);
-  Serial.println("INFO: RESET I2C MULTIPLEXER, READY FOR SAMPLING");
 }
 /***************************************************************************//**
  * @brief  The main execution loop.
  ******************************************************************************/
 void loop() {
   uint32_t timestamp = rtc_get_unix_time(rtc);
-
   Serial.print(timestamp);
   Serial.print(',');
-
-  // READ, WRITE, AND PRINT MOS DATA:
+  
   i2c_select_channel(MOS_SENSORS);
-  Serial.println("INFO: SELECTED MOS SENSORS");
   MOS_ADCS.read_values(MOS_BUFFER);
-  Serial.println("INFO: READ MOS DATA INTO BUFFER");
   serial_write_adc_group_data(MOS_BUFFER, MOS_BUFFER_SIZE);
-  Serial.print("\r\n");
+  Serial.print(',');
+ 
+  i2c_select_channel(NO_SENSORS);
+  ELECTROCHEM_ADCS.read_values(ELECTROCHEM_BUFFER);
+  serial_write_adc_group_data(ELECTROCHEM_BUFFER, ELECTROCHEM_BUFFER_SIZE);
+  Serial.print(',');
+  
+  i2c_select_channel(CO_SENSORS);
+  ELECTROCHEM_ADCS.read_values(ELECTROCHEM_BUFFER);
+  serial_write_adc_group_data(ELECTROCHEM_BUFFER, ELECTROCHEM_BUFFER_SIZE);
+  Serial.print(',');
 
-  // READ, WRITE, AND PRINT ELECTROCHEM DATA:
-  for (uint8_t i = NO_SENSORS; i < CO2_SENSORS; ++i) {
-    i2c_select_channel(i);
-    Serial.println("INFO: SELECTED ELECTROCHEM ADC GROUP");
-    ELECTROCHEM_ADCS.read_values(ELECTROCHEM_BUFFER);
-    Serial.println("INFO: READ ELECTROCHEM DATA INTO BUFFER");
-    serial_write_adc_group_data(ELECTROCHEM_BUFFER, ELECTROCHEM_BUFFER_SIZE);
-  }
-
-  // READ, WRITE, AND PRINT CO2 DATA:
+  i2c_select_channel(OX_SENSORS);
+  ELECTROCHEM_ADCS.read_values(ELECTROCHEM_BUFFER);
+  serial_write_adc_group_data(ELECTROCHEM_BUFFER, ELECTROCHEM_BUFFER_SIZE);
+  Serial.print(',');
+  
   i2c_select_channel(CO2_SENSORS);
-  Serial.println("INFO: SELECTED CO2 ADC GROUP");
   CO2_ADCS.read_values(CO2_BUFFER);
-  Serial.println("INFO: READ CO2 DATA INTO BUFFER");
   serial_write_adc_group_data(CO2_BUFFER, CO2_BUFFER_SIZE);
   Serial.print("\r\n");
-
+  
   delay(1000);
 }
