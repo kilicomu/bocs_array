9ad5f1516eaed358aed0dbb8a8f62328cc7ed5f5
Killian Murphy <killian.murphy@york.ac.uk>
2021-12-16 20:03:17 +0000

    Complete first iteration of extensive simplification

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,13 @@
 /******************************************************************************/
 #define BAUD_RATE 115200
 
-#define I2C_MUX_ADDRESS      0x70
-#define I2C_SENSOR_1_ADDRESS 0x48
-#define I2C_SENSOR_2_ADDRESS 0x49
-#define I2C_SENSOR_3_ADDRESS 0x4A
-#define I2C_SENSOR_4_ADDRESS 0x4B
+#define I2C_MUX_ADDRESS            0x70
+#define I2C_ADC_1_ADDRESS          0x48
+#define I2C_ADC_2_ADDRESS          0x49
+#define I2C_ADC_3_ADDRESS          0x4A
+#define I2C_ADC_4_ADDRESS          0x4B
+#define I2C_POWER_SENSOR_1_ADDRESS 0x40
+#define I2C_POWER_SENSOR_2_ADDRESS 0x41
 
 #define MUX_MOS_SENSOR_CHANNEL  0x00
 #define MUX_NO_SENSOR_CHANNEL   0x01
@@ -34,19 +36,20 @@
 #define ADC_NUM_CO2_ADCS         0x03
 #define ADC_NUM_META_ADCS        0x02
 /******************************************************************************/
-void    i2c_read_power_sensor(Adafruit_INA219 power_sensor, float *values);
+void    i2c_read_channel_adcs(Adafruit_ADS1115 *adcs, int adc_count);
+void    i2c_read_power_sensor(Adafruit_INA219 power_sensor);
 uint8_t mux_select_channel(uint8_t channel);
 /******************************************************************************/
 Adafruit_ADS1115 adcs[4] = {
-  Adafruit_ADS1115(I2C_SENSOR_1_ADDRESS),
-  Adafruit_ADS1115(I2C_SENSOR_2_ADDRESS),
-  Adafruit_ADS1115(I2C_SENSOR_3_ADDRESS),
-  Adafruit_ADS1115(I2C_SENSOR_4_ADDRESS)
+  Adafruit_ADS1115(I2C_ADC_1_ADDRESS),
+  Adafruit_ADS1115(I2C_ADC_2_ADDRESS),
+  Adafruit_ADS1115(I2C_ADC_3_ADDRESS),
+  Adafruit_ADS1115(I2C_ADC_4_ADDRESS)
 };
-Adafruit_INA219 power_sensor, power_sensor_2;
+Adafruit_INA219 power_sensor_1(I2C_POWER_SENSOR_1_ADDRESS);
+Adafruit_INA219 power_sensor_2(I2C_POWER_SENSOR_2_ADDRESS);
 File sd_data_file;
 char sd_filename[12] = {'\0'};
-float power_sensor_values[3];
 RTC_DS1307 rtc;
 bool sd_initialised = false;
 /******************************************************************************/
@@ -69,7 +72,7 @@ void setup() {
 
   // INITIALISE MOS CHANNEL ADCS AND POWER SENSORS:
   mux_select_channel(MUX_MOS_SENSOR_CHANNEL);
-  power_sensor.begin();
+  power_sensor_1.begin();
   power_sensor_2.begin();
   for (uint8_t i = 0; i < ADC_NUM_MOS_ADCS; ++i) {
     adcs[i].begin();
@@ -78,7 +81,7 @@ void setup() {
   // INITIALISE ELECTROCHEM ADCS AND POWER SENSORS:
   for (uint8_t i = MUX_NO_SENSOR_CHANNEL; i < MUX_CO2_SENSOR_CHANNEL; ++i) {
     mux_select_channel(i);
-    power_sensor.begin();
+    power_sensor_1.begin();
     for (uint8_t j = 0; j < ADC_NUM_ELECTROCHEM_ADCS; ++j) {
       adcs[j].begin();
     }
@@ -86,14 +89,14 @@ void setup() {
 
   // INITIALISE CO2 ADCS AND POWER SENSORS:
   mux_select_channel(MUX_CO2_SENSOR_CHANNEL);
-  power_sensor.begin();
+  power_sensor_1.begin();
   for (uint8_t i = 0; i < ADC_NUM_CO2_ADCS; ++i) {
     adcs[i].begin();
   }
 
   // INITIALISE PUMP POWER SENSORS:
   mux_select_channel(MUX_PUMP_SENSOR_CHANNEL);
-  power_sensor.begin();
+  power_sensor_1.begin();
 
   // INITIALISE META ADCS:
   mux_select_channel(MUX_META_SENSOR_CHANNEL);
@@ -116,7 +119,7 @@ void loop() {
     sd_data_file = SD.open(sd_filename, FILE_WRITE);
 
     if (! sd_data_file) {
-      Serial.println("ERROR: UNABLE TO OPEN SD CARD FILE");
+      Serial.println(F("ERROR: UNABLE TO OPEN SD CARD FILE"));
     }
   }
 
@@ -126,16 +129,69 @@ void loop() {
 
   if (sd_initialised) {
     sd_data_file.print(loop_iteration_start.unixtime());
-    sd_data_file.print(",");
+    sd_data_file.print(F(","));
   }
 
-  // SAMPLE DATA FROM ADCS AND POWER SENSORS, WRITING TO SERIAL / SD CARD:
+  // SAMPLE DATA FROM MOS ADCS AND POWER SENSORS:
+  mux_select_channel(MUX_MOS_SENSOR_CHANNEL);
+  i2c_read_power_sensor(power_sensor_1);
+  i2c_read_power_sensor(power_sensor_2);
+  i2c_read_channel_adcs(adcs, ADC_NUM_MOS_ADCS);
+
+  // SAMPLE DATA FROM ELECTROCHEM ADCS AND POWER SENSORS:
+  for (uint8_t i = MUX_NO_SENSOR_CHANNEL; i < MUX_CO2_SENSOR_CHANNEL; ++i) {
+    mux_select_channel(i);
+    i2c_read_power_sensor(power_sensor_1);
+    i2c_read_channel_adcs(adcs, ADC_NUM_ELECTROCHEM_ADCS);
+  }
+
+  // SAMPLE DATA FROM CO2 ADCS AND POWER SENSORS:
+  mux_select_channel(MUX_CO2_SENSOR_CHANNEL);
+  i2c_read_power_sensor(power_sensor_1);
+  i2c_read_channel_adcs(adcs, ADC_NUM_CO2_ADCS);
+
+  // SAMPLE DATA FROM PUMP POWER SENSORS:
+  mux_select_channel(MUX_PUMP_SENSOR_CHANNEL);
+  i2c_read_power_sensor(power_sensor_1);
+
+  // SAMPLE DATA FROM META ADCS:
+  mux_select_channel(MUX_META_SENSOR_CHANNEL);
+
+  uint16_t value_1 = adcs[0].readADC_SingleEnded(0);
+  uint16_t value_2 = adcs[0].readADC_SingleEnded(1);
+
+  Serial.print(value_1);
+  Serial.print(F(","));
+
+  Serial.print(value_2);
+  Serial.print(F(","));
+
+  if (sd_initialised) {
+    sd_data_file.print(value_1);
+    sd_data_file.print(F(","));
+
+    sd_data_file.print(value_2);
+    sd_data_file.print(F(","));
+  }
+
+  value_1 = adcs[1].readADC_SingleEnded(0);
+  value_2 = adcs[1].readADC_SingleEnded(1);
+
+  Serial.print(value_1);
+  Serial.print(F(","));
+  Serial.print(value_2);
+
+  if (sd_initialised) {
+    sd_data_file.print(value_1);
+    sd_data_file.print(F(","));
+    sd_data_file.print(value_2);
+  }
   
   // WRITE LINE SEPARATOR TO SERIAL / SD CARD AND FLUSH SD CARD WRITE BUFFER:
   Serial.print(F("\r\n"));
   
   if (sd_initialised) {
-    sd_data_file.print("\r\n");
+    sd_data_file.print(F("\r\n"));
     sd_data_file.close();
   }
 
@@ -143,11 +199,45 @@ void loop() {
   while ((rtc.now() - loop_iteration_start).totalseconds() < 2) {}
 }
 /******************************************************************************/
-void i2c_read_power_sensor(Adafruit_INA219 power_sensor, float *values) {
-  values[0] = ((float) power_sensor.getBusVoltage_V() * 1000) + 
-    power_sensor.getShuntVoltage_mV();
-  values[1] = power_sensor.getCurrent_mA();
-  values[2] = power_sensor.getPower_mW();
+void i2c_read_channel_adcs(Adafruit_ADS1115 *adcs, int adc_count) {
+  for (uint8_t i = 0; i < adc_count; ++i) {
+    int16_t value_1 = adcs[i].readADC_Differential_0_1();
+    int16_t value_2 = adcs[i].readADC_Differential_2_3();
+
+    Serial.print(value_1);
+    Serial.print(F(","));
+
+    Serial.print(value_2);
+    Serial.print(F(","));
+
+    if (sd_initialised) {
+      sd_data_file.print(value_1);
+      sd_data_file.print(F(","));
+
+      sd_data_file.print(value_2);
+      sd_data_file.print(F(","));
+    }
+  }
+  
+
+}
+
+void i2c_read_power_sensor(Adafruit_INA219 power_sensor) {
+  float values[3] = {
+    ((float) power_sensor.getBusVoltage_V() * 1000) + power_sensor.getShuntVoltage_mV(),
+    power_sensor.getCurrent_mA(),
+    power_sensor.getPower_mW()
+  };
+
+  for (uint8_t i = 0; i < 3; ++i) {
+    Serial.print(values[i]);
+    Serial.print(F(","));
+
+    if (sd_initialised) {
+      sd_data_file.print(values[i]);
+      sd_data_file.print(F(","));
+    }
+  }
 }
 
 uint8_t mux_select_channel(uint8_t channel) {
